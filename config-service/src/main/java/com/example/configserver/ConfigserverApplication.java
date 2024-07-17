5cc10ff68627c410d58b3e921704f857a14d6ec0
Ngô Vũ Minh Đạt <ngovuminhdat@gmail.com>
2024-07-17 17:57:19 +0700

    config(workflow): change name input variable

--- config-service/src/main/java/com/example/configserver/ConfigserverApplication.java
+++ config-service/src/main/java/com/example/configserver/ConfigserverApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigserverApplication {
-		/// testfdsffdsfsad
+		/// testfdsffdsfsadfdsf
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigserverApplication.class, args);
 	}

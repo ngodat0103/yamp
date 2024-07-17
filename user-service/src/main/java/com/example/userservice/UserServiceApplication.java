1036e2e410543287ebe4cc408a2c5088627436a8
Ngô Vũ Minh Đạt <ngovuminhdat@gmail.com>
2024-07-17 09:42:32 +0700

    config(workflow): Test changes

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -47,7 +47,7 @@ public class UserServiceApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-	// has change 3fdfsdfdfdsf
+	// has change 3fdfsdfdfdsffdsfs
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);

a0a81ae6b4113c015b63672d59f25f2744f75be2
Ngô Vũ Minh Đạt <ngovuminhdat@gmail.com>
2024-07-17 16:38:02 +0700

    config(workflow): upgrade to buildx@v3.4.0

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -47,7 +47,7 @@ public class UserServiceApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-	// has change 3fdsaffdafd
+	// has change 3fdsaffdafdfdsf
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}

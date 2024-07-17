374b6c0bd052b4464cbe05b68f5b1efc1d4fdee1
Ngô Vũ Minh Đạt <ngovuminhdat@gmail.com>
2024-07-17 11:06:18 +0700

    config(workflow): fix typo

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -47,7 +47,7 @@ public class UserServiceApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-	// has change 3fdfsdfdfdsffdsfsfdsffsdfsfdsfdfdsfsdfdsffdsfdsad
+	// has change 3fdfsdfdfdsffdsfsfdsffsdfsfdsfdfdsfsdfdsffdsfdsadfdsf
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}

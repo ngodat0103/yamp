e1c503b2547f1bdcfe10cb345a67a2081c3206f1
Ngô Vũ Minh Đạt <ngovuminhdat@gmail.com>
2024-07-19 13:14:49 +0700

    config(workflow-test):  upload step if vul

--- auth-service/src/main/java/org/example/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/org/example/authservice/AuthServiceApplication.java
@@ -9,7 +9,7 @@ public class AuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
-//fdsfddsfsdfdsffdsffdsfsdsadsa
+//fdsfddsfsdfdsffdsffdsfsdsadsafdsf
     }
 
 }

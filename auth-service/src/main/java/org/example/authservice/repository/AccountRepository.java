f2a47e9a347ea99890e6a591f0362d8cca919622
Ngô Vũ Minh Đạt <21521935@gm.uit.edu.vn>
2024-07-31 15:11:21 +0700

    resolved conflict

--- auth-service/src/main/java/org/example/authservice/repository/AccountRepository.java
+++ auth-service/src/main/java/org/example/authservice/repository/AccountRepository.java
@@ -1,16 +1,21 @@
 package org.example.authservice.repository;
-import jakarta.transaction.Transactional;
+
 import org.example.authservice.entity.Account;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends CrudRepository<Account, UUID> {
-    @Transactional
+
+    Account save(Account account);
     Optional<Account> findByUsername(String username);
+    Account findByEmail(String email);
     Account findByAccountUuid(UUID accountUuid);
-    boolean existsByEmail(String email);
     boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    void deleteByUsername(String username);
+
 }
--- auth-service/src/main/java/org/example/authservice/service/AccountService.java
+++ auth-service/src/main/java/org/example/authservice/service/AccountService.java
@@ -1,20 +1,13 @@
 package org.example.authservice.service;
 
 
-import jakarta.transaction.Transactional;
 import org.example.authservice.dto.AccountDto;
-import org.example.authservice.entity.Account;
 
 import java.util.UUID;
 
 public interface AccountService {
 
-    AccountDto getAccount(UUID accountUuid);
-    @Transactional(rollbackOn = NullPointerException.class)
-    Account register(AccountDto accountDto) ;
-    void patchPassword(UUID accountUuid, String newPassword);
-    void patchEmail(UUID accountUuid, String newEmail);
-    void deleteAccount(UUID accountUuid);
-    void addRole(UUID accountUuid, String roleName);
+    AccountDto register(AccountDto accountDto) ;
+    AccountDto getAccount(UUID uuid);
 
 }

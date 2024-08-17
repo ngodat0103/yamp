26563a3a5c20df87af1e192c6461aabe9cf9e7bd
Ngô Vũ Minh Đạt <21521935@gm.uit.edu.vn>
2024-08-17 15:13:39 +0700

    config(ci): set up workflow for remainning svc

---
+++ gateway-svc/src/test/java/com/example/gateway/unit_test/UnitTestHolder.java
@@ -0,0 +1,17 @@
+package com.example.gateway.unit_test;
+
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebFlux;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+
+
+
+
+@WebFluxTest
+@ActiveProfiles("test")
+@AutoConfigureWebFlux()
+public class UnitTestHolder {
+    // todo add later
+}

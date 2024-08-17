a634dff7bc69d31a817accdfe49c83707daea649
Ngô Vũ Minh Đạt <21521935@gm.uit.edu.vn>
2024-08-17 12:12:37 +0700

    config(ci): test

--- auth-svc/src/main/java/org/example/authservice/AuthServiceApplication.java
+++ auth-svc/src/main/java/org/example/authservice/AuthServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
+
+        // fsdfdsfsf
         SpringApplication.run(AuthServiceApplication.class, args);
 
     }

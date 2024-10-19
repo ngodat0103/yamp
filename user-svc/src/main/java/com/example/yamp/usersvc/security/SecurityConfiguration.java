41ba352d622cd3fd8edae5590a7964706711f83d
ngodat0103 <ngovuminhdat@gmail.com>
2024-10-19 16:26:06 +0700

    config(observation-and-monitoring): configure local development with docker compose

--- user-svc/src/main/java/com/example/yamp/usersvc/security/SecurityConfiguration.java
+++ user-svc/src/main/java/com/example/yamp/usersvc/security/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.example.yamp.usersvc.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -42,6 +41,9 @@ public class SecurityConfiguration {
                 .hasAnyRole("ACTUATOR", "ADMIN")
                 .requestMatchers("**/swagger-ui/**")
                 .permitAll()
+                    .requestMatchers( HttpMethod.POST, "/api/v1/users")
+                    .permitAll()
+
                 .anyRequest()
                 .authenticated());
     http.sessionManagement(
